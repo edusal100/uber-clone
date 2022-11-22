c3efcafd9d9167113f57101f8d84e5de06421397
Eduardo Saldana <edusal100@hotmail.com>
2022-11-21 22:32:16 -0600

    Added Google Places Search

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,7 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 
+import { GOOGLE_MAPS_APIKEY } from '@env'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import NavOptions from '../components/NavOptions'
 import React from 'react'
 import tw from 'twrnc'
@@ -10,6 +12,23 @@ const HomeScreen = () => {
       <Image
       style={{width: 100, height:100, resizeMode: 'contain'}}
       source={{uri: 'https://links.papareact.com/gzs'} }
+      />
+      <GooglePlacesAutocomplete
+        placeholder='Where from?'
+        styles={{
+          container: {
+            flex: 0
+          },
+          textInput: {
+            fontSize: 18,
+          }
+        }}
+        query= {{
+          key: GOOGLE_MAPS_APIKEY,
+          language: 'en'
+        }}
+        nearbyPlacesAPI='GooglePlacesSearch'
+        debounce={400}
       />
     <NavOptions/>
     </View>
